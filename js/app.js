2e71fb6ef5484159b15089846238235b5d74e842
glous09 <goyavaes@hotmail.es>
2018-04-04 08:26:52 -0500

    Cambios de diseño y funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,37 +1,21 @@
+$(document).ready(function(){
 
-function loadPage() { //Función que contiene funciones
-  $("#searcher").focus();//Comprobamos si se pulsa una tecla
-  $("#searcher").keyup(filterRestaurant);//Filtra los restaurantes
-}
+  //Agreagando y removiendo clases
+  $('.category-list .item[category="all"]').addClass('item-active');
 
-function paintContactsInHtml (restaurants) {
-var $newRestaurant = $("<div />", {
-  "class": "col-6"
-});
-var $containerRestauranteName = $("<h4 />");
-//var $imgRestaurant = $("img")
-//Agregando elementos al html
-$containerRestauranteName.text(restaurants.name);
-$newRestaurant.append($containerRestauranteName);
-//$imgRestaurant.attr('src','data-name', restaurant.name);
-console.log($newRestaurant);
-$("#img-food").prepend($newRestaurant);
-}
+  $('.item').click(function(){
+    let food = $(this).attr('category');
+    console.log(food);
+    $('.item').removeClass('item-active');
+    $(this).addClass('item-active');
 
-function filterRestaurant (){
-  var searchRestaurant = $("#searcher").val().toLowerCase();
-  if($("#searcher").val().trim().length > 0) {
-    var filteredRestaurant = restaurants.filter(function(restaurants) {
-      return restaurants.name.toLowerCase().indexOf(searchRestaurant) >= 0;
-    });
-    $("#img-food").empty();
-    filteredRestaurant.forEach(function(restaurants){
-      paintRestaurantInHtml(restaurants);
-    });
-  } else {
-    $("#img-food").empty();
-    restaurants.forEach(function(restaurants){
-      paintRestaurantInHtml(restaurants);
-    });
-  }
-  $(document).ready(loadPage);
+    //Ocultando platillos
+    $('.food-item').hide();
+    //Mostrando plaillos
+    $('.food-item[category="'+food+'"]').show();
+  });
+  $('.item[category="all"]').click(function(){
+    $('.food-item').show();
+  })
+
+})
